1f4c579241f350b1ba8ca49c25a0587cd1a7a24f
Jacob Menke <jamenk@email.wm.edu>
2017-01-19 14:47:51 -0500

    Create createTextFile.sh

---
+++ createTextFile.sh
@@ -0,0 +1,57 @@
+executableScriptsProcessing(){
+	# then make it executable
+	chmod 700 "$newfile"
+	#add header
+	echo "#created by JAKOBMENKE --> `date` " >> "$newfile"
+	#and open file
+	open -t "$newfile"
+	#run python3 script with pyautogi commands for keyboard shortcuts
+	python3 /Users/jacobmenke/PycharmProjects/textEditorTwoColumns.py
+}
+
+createTheFile(){
+	#create newfile
+	> "$newfile"
+	
+	#echo shebang line into newfile
+	case "$1" in
+		.sh ) echo "#!/usr/local/bin/bash" > "$newfile" ;;
+		.pl ) echo "#!/usr/local/bin/perl" > "$newfile" ;;
+		.rb ) echo "#!/Users/jacobmenke/.rvm/rubies/ruby-2.3.3/bin/ruby" > "$newfile" ;;
+		.py ) echo "#!/Library/Frameworks/Python.framework/Versions/3.5/bin/python3" > $newfile ;;
+		#if .txt then just open the file, this is the case for plain text files
+		#exit so do not call executableScriptsProcessing
+		* 	) open -t "$newfile"; exit;;
+	esac
+
+	executableScriptsProcessing
+	
+}
+
+#if no arguments then exit
+if [[ -z "$1" ]]; then
+	printf "I need an argument ...\n"
+	exit 1
+fi
+#file name is the first argument
+newfile="$1"
+
+#check ending on file name and call createTheFile passing in argument for file ending
+if [[ "$newfile" =~ .*.sh ]]; then
+	createTheFile .sh
+elif [[ "$newfile" =~ .*.pl ]]; then
+	createTheFile .pl
+elif [[ "$newfile" =~ .*.rb ]]; then
+	createTheFile .rb
+elif [[ "$newfile" =~ .*.py ]]; then
+	createTheFile .py
+elif [[ "$newfile" =~ .*.txt ]]; then  #.txt
+	createTheFile .txt text
+else
+	#if no file ending default to text file
+	#construct file with ending of .txt
+	#call createTheFile with 2 arguments
+	newfile="$newfile".txt
+	createTheFile .txt text
+fi
+
